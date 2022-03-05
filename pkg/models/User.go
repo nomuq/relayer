49634270d7bd3a33d206de294711e73e3ab46281
Satish Babariya <satish.babariya@gmail.com>
2022-03-05 22:42:01 +0530

    update models

--- pkg/models/User.go
+++ pkg/models/User.go
@@ -1,6 +1,6 @@
 package models
 
 type User struct {
-	ID       string `json:"id"`
-	Username string `json:"username"`
+	ID       string `json:"id" db:"id,omitempty"`
+	Username string `json:"username" db:"username"`
 }
