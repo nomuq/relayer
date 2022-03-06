228e9fdefb8411bcfb99f9a7dd7c1540fd7d7e6c
Satish Babariya <satish.babariya@gmail.com>
2022-03-07 00:36:28 +0530

    add copyright

--- pkg/db/adapter.go
+++ pkg/db/adapter.go
@@ -1,8 +1,28 @@
+/*
+ * (C) Copyright 2022 Satish Babariya (https://satishbabariya.com/) and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Contributors:
+ *     satish babariya (satish.babariya@gmail.com)
+ *
+ */
+
 package db
 
 import "context"
 
-// generic database adapter interface for databases like postgresql, mysql, etc.
+// Adapter generic database adapter interface for databases like postgresql, mysql, etc.
 type Adapter interface {
 	Open(ctx context.Context) error
 	Close()
