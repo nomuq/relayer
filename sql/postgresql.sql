f027b6b66cbd2f3cf75a4364b5412f0bda952ccc
Satish Babariya <satish.babariya@gmail.com>
2022-03-09 12:19:08 +0530

    add initial schema for db

--- sql/postgresql.sql
+++ sql/postgresql.sql
@@ -22,14 +22,58 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- Drop all existing tables [TODO: Remove this once we have a migration tool]
-DROP TABLE IF EXISTS "public"."users";
+
+-- Drop all existing tables with cascade
+DROP TABLE IF EXISTS "public"."users" CASCADE;
+DROP TABLE IF EXISTS "public"."channels" CASCADE;
+DROP TABLE IF EXISTS "public"."channel_members" CASCADE;
+DROP TABLE IF EXISTS "public"."messages" CASCADE;
+
+-- Drop all types
+DROP TYPE IF EXISTS "public"."message_type" CASCADE;
+
 
 -- Set Time Zone to UTC
 SET TIME ZONE 'UTC';
 
-CREATE TABLE IF NOT EXISTS users (
-  id UUID PRIMARY KEY DEFAULT Uuid_generate_v4(), 
-  username TEXT, 
-  created_at TIMESTAMP NOT NULL DEFAULT Now(), 
-  updated_at TIMESTAMP NOT NULL DEFAULT Now()
+-- users
+CREATE TABLE users (
+    id TEXT NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    metadata JSONB
 );
+
+-- channels
+CREATE TABLE channels (
+    id TEXT NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    metadata JSONB
+);
+
+-- channel_members
+CREATE TABLE channel_members (
+    channel_id TEXT NOT NULL REFERENCES channels(id) ON DELETE CASCADE,
+    user_id TEXT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (channel_id, user_id)
+);
+
+-- message types (enum)
+CREATE TYPE message_type AS ENUM ('text', 'file');
+
+-- messages
+CREATE TABLE messages (
+    id TEXT NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    deleted_at TIMESTAMP WITH TIME ZONE,
+    content TEXT NOT NULL,
+    type message_type NOT NULL DEFAULT 'text',
+    metadata JSONB,
+    channel_id TEXT NOT NULL REFERENCES channels(id) ON DELETE CASCADE
+);
+
